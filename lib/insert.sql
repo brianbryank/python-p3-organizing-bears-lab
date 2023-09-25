9a20d5ac552224a0a2ad249ff5be9ae4c0f12aad
brianbryank <brianbryan0125@gmail.com>
2023-09-25 21:45:28 +0300

    passed

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (id,name, age, sex, color, temperament, alive)
+VALUES (1,'Mr. Chocolate', 23, 'M', 'Brown', 'Sanguine', True);
+
+INSERT INTO bears
+VALUES (2,NULL, 23, 'F', 'Brown', 'Sanguine', True);
+
+INSERT INTO bears
+VALUES (3,'Wendy', 23, 'F', 'Brown', 'Sanguine', FALSE);
+
+INSERT INTO bears
+VALUES (4,'Rowdy', 23, 'M', 'Brown', 'Sanguine', True);
+
+INSERT INTO bears
+VALUES (5,'Tabitha', 23, 'F', 'Brown', 'Sanguine', FALSE);
+
+INSERT INTO bears
+VALUES (6,'Sergeant Brown', 23, 'M', 'Brown', 'Sanguine', True);
+
+INSERT INTO bears
+VALUES (7,'Melissa', 23, 'F', 'Brown', 'Sanguine', FALSE);
+
+INSERT INTO bears
+VALUES (8,'Grinch', 23, 'M', 'Brown', 'Sanguine', True);
